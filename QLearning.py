badd92c8d82a788c6e12d3f8c90dcbbe3b35fc26
Matthew Lee <matt_lee92@hotmail.co.uk>
2018-12-01 16:00:52 +0000

    Splitting into RL functions

--- QLearning.py
+++ QLearning.py
@@ -3,6 +3,7 @@ import numpy as np
 import random
 import json
 import utils
+import  csv
 
 #TODO - How to load different missions? Answer: see the utils.setupEnv function
 
@@ -13,7 +14,7 @@ class QLearningAgent(object):
         self.gamma = gamma
         self.epsilon_min = 0.01
         self.epsilon = epsilon
-        self.epsilon_decay = 0.99999
+        self.epsilon_decay = 0.999
         self.training = True if self.epsilon > 0 else False
 
         # Don't consider waiting action
@@ -27,41 +28,81 @@ class QLearningAgent(object):
         else:
             # Initialise the Q-Table from blank
             self.qTable = {}
+        return
 
-    def runAgent(self, env):
+    def startGame(self,env, i):
+        print(" ------- New Game ----------  \n")
+        #Store the Q-Table as a JSON
+        print("Saving QTable as JSON")
+        with open('QTable.json', 'w') as fp:
+            json.dump(self.qTable, fp)
 
-        for i in range(1000):
-
-            print(" ------- New Game ----------  \n")
-
-            #Store the Q-Table as a JSON
-            print("Saving QTable as JSON")
-            with open('QTable.json', 'w') as fp:
+        if (i+1) % 10 == 0:
+            print("Saving QTable BackUp as JSON")
+            # Store a QTable BackUp too every 10 games
+            with open('QTableBackUp.json', 'w') as fp:
                 json.dump(self.qTable, fp)
 
-            if (i+1) % 10 == 0:
-                print("Saving QTable BackUp as JSON")
-                # Store a QTable BackUp too every 10 games
-                with open('QTableBackUp.json', 'w') as fp:
-                    json.dump(self.qTable, fp)
+        # Initialise the MineCraft environment
+        obs = env.reset()
+        # Do an initial 'stop' step in order to get info from env
+        obs, currentReward, done, info = env.step(0)
 
-            # Initialise the MineCraft environment
-            obs = env.reset()
-            # Do an initial 'stop' step in order to get info from env
-            obs, currentReward, done, info = env.step(0)
+        # Use utils module to discretise the info from the game
+        [xdisc, ydisc, zdisc, yawdisc, pitchdisc] = utils.discretiseState(info['observation'])
+        currentState = "%d:%d:%d" % (xdisc, zdisc, yawdisc)
+        print("initialState: " + currentState)
+        return currentState, info
 
-            print("Info: ")
-            print(info)
 
-            # Use utils module to discrete the info from the game
-            [xdisc, ydisc, zdisc, yawdisc, pitchdisc] = utils.discretiseState(info['observation'])
-            currentState = "%d:%d:%d" % (xdisc, zdisc, yawdisc)
-            print("currentState: " + currentState)
-
+    def runAgent(self,env):
+        results = []
+        for i in range(1000):
+            currentState, info = self.startGame(env,i)
+            actionCount = 0
+            score = 0
+            done  = False
             while not done:
-                done, currentState, info = self.act(env, currentState, info)
-                #currentState = newState
-                #info = newInfo
+                # Chose the action then run it
+                action = self.act(env, currentState, info)
+                obs, reward, done, info = env.step(action)
+                # Continue counts of actions and scores
+                actionCount += 1
+                score += reward
+                # Check if game is done
+                if done:
+                    break
+                # have to use this to keep last info for results
+                oldInfo = info
+                # Use utils module to discrete the info from the game
+                [xdisc, ydisc, zdisc, yawdisc, pitchdisc] = utils.discretiseState(info['observation'])
+                newState = "%d:%d:%d" % (xdisc, zdisc, yawdisc)
+
+                print('Q-Value for Current State: ')
+                print(self.qTable[currentState])
+
+                # If no Q Value for this state, Initialise
+                if newState not in self.qTable:
+                    self.qTable[newState] = ([0] * len(self.actions))
+
+                # update Q-values for this action
+                if self.training:
+                    oldQValueAction = self.qTable[currentState][self.actions.index(action)]
+                    self.qTable[currentState][self.actions.index(action)] = oldQValueAction + self.alpha * (reward + self.gamma * max(
+                        self.qTable[newState]) - oldQValueAction)
+                currentState = newState
+
+            print(' ------- Game Finished ----------  \n')
+            results.append([score,actionCount,oldInfo['observation']['TotalTime'], self.epsilon])
+            # Decay the epsilon until the minimum
+            if self.epsilon > self.epsilon_min:
+                self.epsilon *= self.epsilon_decay
+            else:
+                self.epsilon = 0
+        with open(f"{self.__name__}.csv","w") as f:
+            wr = csv.writer(f)
+            wr.writerows(results)
+        return results
 
 
     def act(self, env, currentState, info):
@@ -85,47 +126,13 @@ class QLearningAgent(object):
             action = self.actions[np.argmax(currentStateActions)]
 
             print("From State %s (X,Z,Yaw), taking q action: %s" % (currentState,  action))
-
-        # Plays an action
-        new_state, currentReward, done, info = env.step(action)
-
-        #TODO - Check this reward - what is it??
-
-        # Check if the game is finished
-        if done:
-            print(' ------- Game Finished ----------  \n')
-            return done, currentState, info
-
-        # Use utils module to discrete the info from the game
-        [xdisc, ydisc, zdisc, yawdisc, pitchdisc] = utils.discretiseState(info['observation'])
-        newState = "%d:%d:%d" % (xdisc, zdisc, yawdisc)
-
-        print('Q-Value for Current State: ')
-        print(self.qTable[currentState])
-
-        # If no Q Value for this state, Initialise
-        if newState not in self.qTable:
-            self.qTable[newState] = ([0] * len(self.actions))
-
-        # update Q-values for this action
-        if self.training:
-            oldQValueAction = self.qTable[currentState][self.actions.index(action)]
-            self.qTable[currentState][self.actions.index(action)] = oldQValueAction + self.alpha * (currentReward + self.gamma * max(
-                self.qTable[newState]) - oldQValueAction)
-
-            # Decay the epsilon until the minimum
-            if self.epsilon > self.epsilon_min:
-                self.epsilon *= self.epsilon_decay
-            else:
-                self.epsilon = 0
-        return done, newState, info
-
+        return action
 
 
 def main():
     env = utils.setupEnv('find_the_goal_mission2.xml')
     # Get the number of available actions, minus waiting action
-    actionSize = env.action_space.n 
+    actionSize = env.action_space.n
 
     # Give user decision on loadind model or not
     load = input("Load Q Table? y/n - Default as y:________")
@@ -138,6 +145,7 @@ def main():
 
     # Start the running of the Agent
     myAgent.runAgent(env)
+
     return
 
 if __name__ == "__main__":
