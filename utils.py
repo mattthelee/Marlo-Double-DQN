4567f87369515efc5fd0257d9200d46aa63c9a49
BenSaunders27 <benjaminsaunders27@gmail.com>
2018-12-07 23:25:42 +0000

    small changes

--- utils.py
+++ utils.py
@@ -75,6 +75,8 @@ def discretiseState(obs,toNearest = [0.5,45]):
 def completeAction(env,action):
     # Actions do not always take effect immediately, therefore do an action and wait for state change before returning
     # Because Marlo does not provide the reward for the action just taken but for the previous action, need to do wait action before
+
+    """
     image, reward1, done, info = env.step(0)
     sleep(0.1)
     image, reward2, done, info = env.step(action)
@@ -90,6 +92,25 @@ def completeAction(env,action):
         image, reward3, done, info = env.step(action)
         reward += reward3
         return image, reward, done, info['observation']
+
+    """
+    image, reward, done, info = env.step(0)
+    image, reward, done, info = env.step(action)
+    if done:
+        return image, reward, done, info['observation']
+    # Check ten times if action has been completed
+    obs = info['observation']
+    for i in range(10):
+        # If the game is over the observation history is 0
+        if len(env._get_world_state().observations) == 0:
+            print("Found Goal")
+            reward = 0.5
+            done = True
+            break
+        obs = json.loads(env._get_world_state().observations[-1].text)
+        if actionCompleted(info['observation'], obs, action):
+            break
+    return image, reward, done, obs
 
 def actionCompleted(obs1,obs2,action):
     # If action is not a movement return false
