5abee8dd1a9cfd061739a34c1c449a108e4137f8
Matthew Lee <matt_lee92@hotmail.co.uk>
2018-12-07 11:56:29 +0000

    Fixing incorrect changes

--- mc.py
+++ mc.py
@@ -9,13 +9,15 @@ import  csv
 
 class MC_agent(object):
 
-    def __init__(self, actions, loadQTable = False, epsilon=1.0, alpha=0.1, training = True):
+    def __init__(self, actions, QTableName = 'mc_QTable.json', CSVName = 'mc_qlearningResults.csv',loadQTable = False, epsilon_decay=0.99, epsilon=1.0, alpha=0.1, gamma = 0.9, training = True):
         self.alpha = alpha
         self.epsilon_min = 0.01
-        self.gamma = 0.01
+        self.gamma = gamma
         self.epsilon = epsilon
-        self.epsilon_decay = 0.99
+        self.epsilon_decay = epsilon_decay
         self.training = training
+        self.QTableName = QTableName
+        self.CSVName = CSVName
 
         # Don't consider waiting action
         self.actions = [i for i in range(1,actions)]
@@ -34,7 +36,15 @@ class MC_agent(object):
 
     def startGame(self,env, i):
         print(" ------- New Game ----------  \n")
-
+        #Store the Q-Table as a JSON
+        print("Saving mc_QTable as JSON")
+        with open(self.QTableName, 'w') as fp:
+            json.dump(self.qTable, fp)
+         if (i+1) % 10 == 0:
+            print("Saving mc_QTable BackUp as JSON")
+            # Store a QTable BackUp too every 10 games
+            with open('mc_QTableBackUp.json', 'w') as fp:
+                json.dump(self.qTable, fp)
         # Initialise the MineCraft environment
         obs = env.reset()
         # Do an initial 'stop' step in order to get info from env
@@ -42,7 +52,7 @@ class MC_agent(object):
 
         # Use utils module to discretise the info from the game
         [xdisc, ydisc, zdisc, yawdisc, pitchdisc] = utils.discretiseState(info['observation'])
-        currentState = "%d:%d:%d" % (xdisc, zdisc, yawdisc)
+        currentState = "%d:%d:%d:%d:%d" % (xdisc, zdisc, yawdisc, ydisc, pitchdisc)
         print("initialState: " + currentState)
         return currentState, info
 
@@ -51,7 +61,8 @@ class MC_agent(object):
         results = []
         states_count = {}
 
-        for i in range(1000):
+        for i in range(200):
+            print("Game " + str(i))
             currentState, info = self.startGame(env,i)
             actionCount = 0
             score = 0
@@ -77,7 +88,7 @@ class MC_agent(object):
                 oldObs = obs
                 # Use utils module to discrete the info from the game
                 [xdisc, ydisc, zdisc, yawdisc, pitchdisc] = utils.discretiseState(obs)
-                newState = "%d:%d:%d" % (xdisc, zdisc, yawdisc)
+                newState = "%d:%d:%d:%d:%d" % (xdisc, zdisc, yawdisc, ydisc, pitchdisc)
 
                 if newState not in states_count:
                     states_count[newState] = ([0] * len(self.actions))
@@ -112,7 +123,7 @@ class MC_agent(object):
                 self.epsilon *= self.epsilon_decay
             else:
                 self.epsilon = 0
-            with open("mclearningResults.csv","w") as f:
+            with open(self.CSVName,"w") as f:
                 wr = csv.writer(f)
                 wr.writerows(results)
         return results
