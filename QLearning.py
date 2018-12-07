f619c5e906944d7274f6898828330eeae1297e34
Matthew Lee <matt_lee92@hotmail.co.uk>
2018-12-07 11:11:38 +0000

    Fix no reward for action by doubling actions

--- QLearning.py
+++ QLearning.py
@@ -11,13 +11,13 @@ import pdb;
 
 class QLearningAgent(object):
 
-    def __init__(self, actions, episodes = 200, QTableName = 'QTable.json', CSVName = 'qlearningResults.csv', loadQTable = False, epsilon_decay=0.99, alpha=0.8, gamma=0.9,epsilon = 1.0 ):
+    def __init__(self, actions, episodes = 200, QTableName = 'QTable.json', CSVName = 'qlearningResults.csv', loadQTable = False, epsilon_decay=0.99, alpha=0.8, gamma=0.9,epsilon = 1.0, training = True ):
         self.alpha = alpha
         self.gamma = gamma
         self.epsilon_min = 0.01
         self.epsilon = epsilon
         self.epsilon_decay = epsilon_decay
-        self.training = True if self.epsilon > 0 else False
+        self.training = training
         # Don't consider waiting action
         self.actions = [i for i in range(1,actions)]
         self.QTableName = QTableName
@@ -39,16 +39,14 @@ class QLearningAgent(object):
         for i in range(self.episodes):
             print("Game: " + str(i))
             print("Epsilon: " + str(self.epsilon))
-            currentState, info = self.startGame(env,i)
+            currentState, obs = self.startGame(env,i)
             actionCount = 0
             score = 0
             done  = False
             while not done:
                 # Chose the action then run it
-                action = self.act(env, currentState, info)
+                action = self.act(env, currentState)
                 image, reward, done, obs = utils.completeAction(env,action)
-
-                obs, reward, done, info = env.step(action)
                 # Continue counts of actions and scores
                 actionCount += 1
                 score += reward
@@ -59,12 +57,12 @@ class QLearningAgent(object):
                         oldQValueAction = self.qTable[currentState][self.actions.index(action)]
                         self.qTable[currentState][self.actions.index(action)] = oldQValueAction + self.alpha * (
                                     reward - oldQValueAction)
-                        print("Reward of %s added to the Q-Table at %s with action %s"  % (str(reward),currentState,action))
+                        print(f"Reward of {reward}")
 
                     break
 
                 # have to use this to keep last info for results
-                oldInfo = info
+                oldObs = obs
                 # Use utils module to discrete the info from the game
                 [xdisc, ydisc, zdisc, yawdisc, pitchdisc] = utils.discretiseState(obs)
                 newState = "%d:%d:%d:%d:%d" % (xdisc, zdisc, yawdisc, ydisc, pitchdisc)
@@ -80,11 +78,11 @@ class QLearningAgent(object):
                 if self.training:
                     oldQValueAction = self.qTable[currentState][self.actions.index(action)]
                     self.qTable[currentState][self.actions.index(action)] = oldQValueAction + self.alpha * (reward + self.gamma * max(self.qTable[newState]) - oldQValueAction)
-                    print("Reward of %s added to the Q-Table at %s with action %s" % (str(reward), currentState, action))
+                    print(f"Reward of {reward}")
                 currentState = newState
 
             print('\n ------- Game Finished ----------  \n')
-            results.append([score,actionCount,oldInfo['observation']['TotalTime'], self.epsilon])
+            results.append([score,actionCount,oldObs['TotalTime'], self.epsilon])
             # Decay the epsilon until the minimum
             if self.epsilon > self.epsilon_min:
                 self.epsilon *= self.epsilon_decay
@@ -94,10 +92,6 @@ class QLearningAgent(object):
             with open(self.CSVName,"w") as f:
                 wr = csv.writer(f)
                 wr.writerows(results)
-            with open('historyDebug',"w") as f:
-                wr = csv.writer(f)
-                wr.writerows(history)
-
         return results
 
 
@@ -126,7 +120,7 @@ class QLearningAgent(object):
         return currentState, info
 
 
-    def act(self, env, currentState, info):
+    def act(self, env, currentState):
 
         # If no Q Value for this state, Initialise
         if currentState not in self.qTable:
